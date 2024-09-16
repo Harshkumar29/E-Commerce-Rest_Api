989bc935314ee126ed6ef3fe5fa70f1bd68e710d
= <harshkumarver@gmail.com>
2024-09-16 20:04:36 +0530

    add modals

---
+++ src/main/java/pro1/E_Commerce/Entities/Products.java
@@ -0,0 +1,20 @@
+package pro1.E_Commerce.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Products {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private double price;
+    private int quantity;
+    @ManyToOne
+    private Category category;
+}
